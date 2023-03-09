6792846f44f34786a6859f47ffce792c70455f8f
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2023-03-09 23:45:08 +0200

    More debug prints

--- migration_engine.py
+++ migration_engine.py
@@ -4,6 +4,7 @@ import os
 _path_here = os.getcwd()
 print(_path_here)
 print(os.listdir("."))
+print(os.listdir(".."))
 _DB_FILE = "persistence/bot_db.sqlite"
 _MIGRATION_FILE_FOLDER = "db/migrations"
 _con = sqlite3.connect(f"{_path_here}/{_DB_FILE}")
